2b52f9af4d2b7c50c312371729a428af18a177e6
alexandergm <sasha_261@outlook.com>
2022-08-18 23:44:37 +0300

    Complete Task 2

--- Homework/Task2/Program.cs
+++ Homework/Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("Максимальное число: " + a);
+    Console.WriteLine("Минимальное число: " + b);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + b);
+    Console.WriteLine("Минимальное число: " + a);
+}
