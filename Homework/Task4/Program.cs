e0cfb43aeb77f5fc00ed873b5630dae19a2ad1b6
alexandergm <sasha_261@outlook.com>
2022-08-18 23:51:53 +0300

    Complete Task 4

--- Homework/Task4/Program.cs
+++ Homework/Task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+Test(a);
+Test(b);
+Test(c);
+
+Console.WriteLine("Максимальное число это - " + max);
+
+void Test(int count)
+{
+    if(count > max) max = count;
+}
