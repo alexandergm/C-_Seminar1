69d4375dcc25664946de7cadbfe24a9d4f0b2b83
alexandergm <sasha_261@outlook.com>
2022-08-18 23:55:03 +0300

    Complete Task 6

--- Homework/Task6/Program.cs
+++ Homework/Task6/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0) Console.WriteLine("Число " + a + " чётное.");
+else Console.WriteLine("Число " + a + " не чётное.");
